369c1a05c21891146f7386e140d10cc239ab1d8e
hubert <hubert77@gmail.com>
2013-07-18 16:59:35 -0700

    Add helper_methods for current_account and current_account_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_account
+    Account.find(1)
+  end
+  helper_method :current_account
+
+  def current_account_id
+    1
+  end
+  helper_method :current_account_id
 end
